724420abd00ce22c1638704321e0a85d51658d74
s.dinmuhametov <perfog@mail.ru>
2019-11-13 14:44:29 +0300

    readta pp

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+
+var file_container = document.querySelector(".files");
+
+// function ItemImg(props){
+//     return (<img src="img/file.svg"/>)
+// }
+
+
+
+const itemImg = {
+
+};
+
+// ReactDOM.render(
+//     <ItemImg />,
+//     file_container
+// );
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  file_container
+);
